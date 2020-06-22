8862df46860819239dda475bd21703f57be9934d
Rohitstar2050 <32133448+Rohitstar2050@users.noreply.github.com>
2020-06-22 19:25:14 +0530

    Add files via upload

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,7 +5,12 @@ from  . import views
 
 urlpatterns = [
     path(r'',views.index,name='index'),
+    path(r'home',views.index,name='index'),
     path(r'gallery',views.gallery,name='gallery'),
+    path(r'aboutus',views.aboutus,name='aboutus'),
+    path(r'contact',views.contact,name='contact'),
+    path(r'services',views.services,name='services'),
+    path(r'blog',views.blog,name='blog'),
 
 
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -8,3 +8,13 @@ def index(request):
 
 def gallery(request):
     return render(request,'gallery.html')
+
+def blog(request):
+    return render(request,'blog.html')
+def services(request):
+    return render(request,'services.html')
+def contact(request):
+    return render(request,'contact.html')
+
+def aboutus(request):
+    return render(request,'aboutus.html')
