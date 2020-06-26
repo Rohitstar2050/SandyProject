333ed4bc06acda8776d9bf774f7094069bde956d
Rohitstar2050 <32133448+Rohitstar2050@users.noreply.github.com>
2020-06-26 21:05:36 +0530

    Add files via upload

---
+++ static/js/contact.js
@@ -0,0 +1,6 @@
+function SubFunction(){
+var name=document.getElementsByName("name")[0].innerText
+var email=document.getElementsByName("email")[0].innerText
+var msg=document.getElementsByName("msg")[0].innerText
+alert(name)
+}
