cfef0356ccbc505ebe1ff210c9b44acdfb017c76
Rohitstar2050 <32133448+Rohitstar2050@users.noreply.github.com>
2020-07-02 10:43:11 +0530

    Add files via upload

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import contactdata
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # Create your views here.
+def sendMail(name,email,msg):
+    mail_content = '''Filled details are mentioned below:
+                      Name:'''+name+'''
+                      Email ID:'''+email+'''
+                      Message:'''+msg+'''
+                      
+                      Note:do not reply this is system generated mail.
+                    
+    '''
+    # The mail addresses and password
+    sender_address = 'pnitech20@gmail.com'
+    sender_pass = '882838916388'
+    receiver_address = 'pnitech20@gmail.com'
+    # Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'Mail from Webserver PNI TECH'  # The subject line
+    # The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    # Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+    session.starttls()  # enable security
+    session.login(sender_address, sender_pass)  # login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
 
 
 def index(request):
@@ -32,7 +62,9 @@ def contactsubmit(request):
         validate['validationpass']=True
         ref=contactdata(name=name,emailid=email,msg=msg)
         ref.save()
+        sendMail(name,email,msg)
         return render(request,'contact.html',validate)
+        # return redirect('contact')
     else:
         validate['validationfail'] =True
         return render(request,'contact.html',validate)
